6c4d94a1ab8bdcf722420b9758690f0fe7cc9356
Gbetondji Dovonon <jsadovonon@gmail.com>
2021-02-04 16:02:53 +0000

    complete tasks

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 from utils import read_data
-#from model import RuleBasedModel
+from model import RuleBasedModel
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 
 def main():
+    random.seed(0)
 
     train_file = './train_data.txt'
     test_file = './test_data.txt'
     variables = ['ID', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'class']
 
-
     print ("========= Reading train dataset =========")
+    # use the read data function you created to read the train data
     train_data = read_data(train_file,train = True, variables = variables)
-    print(train_data[0])
-	# use the read data function you created to read the train data
+	
     print ("======== Done reading =========.\n")
 
     print ("========= Reading test data =========")
-    test_data = read_data(test_file,train = False, variables = variables) 
-    print(test_data[0])
+    test_data = read_data(test_file,train = True, variables = variables) 
 	# Read the test  data
     print ("========= Done reading =========.\n")
 
     print ("==== Training classifier =====")
-	# TO-DO
-	# Initialize the classifier you built in model.py and return the necessary values
+    # Initialize the classifier you built in model.py and return the necessary values
+    random.shuffle(train_data)
+    model = RuleBasedModel()
+    acc = model.train(train_data)
+    print("Train accuracy is:",acc)
+	
     print ("======== Done training classifier ===========.\n")
 
     print ("========= Classifying test samples =======")
-	# TO-DO 
-	# use your classifier to do predictions on all the test samples
+    # use your classifier to do predictions on all the test samples
+    #test_result = model.test(test_data) 
+	
     print ("========== Done classifying =======")
-
-    	# TO-DO
-	# Evalutate your classifier with the Accuracy function you implemented and return the necessary outputs
-    print(f"Model's Accuracy {round(accuracy)} %, model correctly predicted {numCorrect} out of {total_samples}")
+    # Evalutate your classifier with the Accuracy function you implemented and return the necessary outputs
+    accuracy, numCorrect, total_samples = model.accuracy(test_data)
+    print(f"Model's Accuracy {round(accuracy*100)} %, model correctly predicted {numCorrect} out of {total_samples}")
     print('================================================================')
 
 
---
+++ model.py
@@ -0,0 +1,87 @@
+def space(start,end,nvals):
+    """
+    python implemetation of the np.linspace function using generators
+    """
+    step = (end-start)/nvals
+    for i in range(nvals):
+        yield start+(i*step)
+
+class RuleBasedModel:   
+    """
+    Implemenation of a rule based classifier.
+    Uses a threshold on three variables: f1, f3, f8. Those were the three variables with the highest correlation values with respect to the class
+    """
+    def __init__(self):
+        """
+        Initialize the threshold range to search for values for the optimal threshold
+        Initialize the list of features to use
+        Initialize an empty threshold list. This list will be filled with the three optimal thresholds after training
+        Initialize the acc
+        """
+        self.threshold_ranges = [[0.1,0.4],[0.1,0.5],[0.1,0.5]]
+        self.features = ['f1','f3','f8']
+        self.threshold = [0,0,0]
+        self.acc = 0
+    
+    def pred(self,x, threshold = [0,0,0]):
+        """
+        Function to predict for a single dictionary row
+        Uses the provided threshold or the default threshold if not provided
+        """    
+        if threshold == [0,0,0]:
+            threshold = self.threshold 
+        if x[self.features[0]] > threshold[0] and x[self.features[1]] > threshold[1] and x[self.features[2]] > threshold[2]:
+            return 1.
+        else:
+            return 2.
+
+    def train(self, data):
+        """
+        Train, by searching through the range space in order to find the combination of thresholds that gives the best accuracy
+        """
+        accuracy = 0
+        pair = [0,0]
+        for x in space(self.threshold_ranges[0][0],self.threshold_ranges[0][1],100):
+            for y in space(self.threshold_ranges[1][0],self.threshold_ranges[1][1],100):
+                for z in space(self.threshold_ranges[2][0],self.threshold_ranges[2][1],100):
+                    temp_pair = [x,y,z]
+                    temp_accuracy, a, b = self.accuracy(data,temp_pair)
+                    if temp_accuracy > accuracy:
+                        accuracy = temp_accuracy
+                        pair = temp_pair
+        self.threshold = pair
+        self.acc = accuracy
+        return self.acc
+
+    def test(self, data):
+        """
+        used to predict on data that does not have class labels
+        """
+        result = []
+        for data_point in data:
+            result.append(self.pred(data_point))
+        return result
+
+    def predict_with_id(self, id, data):
+        """
+        Predict for a specific ID
+        """
+        for x in data:
+            if id == data["ID"]:
+                return self.pred(data)
+        else:
+            print("ID not found")
+
+
+    def accuracy(self,data,threshold=[0,0,0]):
+        """
+        Uses trained threshold to predict on data and get the accuracy
+        """
+        if threshold == [0,0,0]:
+            threshold = self.threshold  
+        correct = 0
+        total = len(data)
+        for data_point in data:
+            if data_point["class"] == self.pred(data_point,threshold):
+                correct+=1
+        return correct/total, correct, total
